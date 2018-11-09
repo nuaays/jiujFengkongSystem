9e0a312af25795bf017b480bef96c9a15530f623
qcyin <785128850@qq.com>
2018-11-09 11:25:01 +0800

    java

--- src/main/java/com/zl/controller/IndInfoController.java
+++ src/main/java/com/zl/controller/IndInfoController.java
@@ -1,5 +1,29 @@
 package com.zl.controller;
 
-public class IndInfoController {
+import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.zl.controller.base.BaseController;
+import com.zl.service.IIndInfoService;
+
+@Controller
+@RequestMapping("/IndInfo")
+public class IndInfoController extends BaseController{
+
+	@Autowired
+	private IIndInfoService indInfoService;
+	
+	@RequestMapping("/queryIndInfo.action")
+	public ModelAndView queryIndInfoByCustomerid(String customerid) {
+		ModelAndView mav = new ModelAndView();
+		HashMap<String, Object> modelMap = new HashMap<String,Object>();
+		modelMap.put("indInfo", indInfoService.findIndInfo(customerid));
+		mav.addAllObjects(modelMap);
+		mav.setViewName("zs_geren");
+		return mav;
+	}
 }
--- src/main/java/com/zl/service/IIndInfoService.java
+++ src/main/java/com/zl/service/IIndInfoService.java
@@ -1,5 +1,13 @@
 package com.zl.service;
 
+import com.zl.pojo.IndInfo;
+
 public interface IIndInfoService {
 
+	/**
+	 * 根据customerid查询个人基本信息
+	 * @param customerid
+	 * @return
+	 */
+	IndInfo findIndInfo(String customerid);
 }
