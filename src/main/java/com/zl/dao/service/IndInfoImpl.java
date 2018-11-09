34a97899aab0cf99e17f7d0df611de63bcda3f72
qcyin <785128850@qq.com>
2018-11-09 09:20:33 +0800

    update

--- src/main/java/com/zl/dao/service/IndInfoImpl.java
+++
@@ -1,5 +0,0 @@
-package com.zl.dao.service;
-
-public class IndInfoImpl implements IIndInfoService{
-
-}
---
+++ src/main/java/com/zl/dao/service/impl/IndInfoImpl.java
@@ -0,0 +1,7 @@
+package com.zl.dao.service.impl;
+
+import com.zl.dao.service.IIndInfoService;
+
+public class IndInfoImpl implements IIndInfoService{
+
+}
