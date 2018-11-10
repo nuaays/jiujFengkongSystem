2e39a02df1924f6ec35b1ee9ea41d5dad824682d
THINK <THINK@THINK-Lx>
2018-11-10 10:35:36 +0800

    code

--- src/main/java/com/zl/controller/LoginUserController.java
+++ src/main/java/com/zl/controller/LoginUserController.java
@@ -1,8 +1,13 @@
 package com.zl.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,23 +16,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.zl.code.GraphicHelper;
 import com.zl.pojo.LoginUser;
 import com.zl.service.LoginUserService;
+
 
 
 @Controller
 @RequestMapping("user")
 public class LoginUserController {
-	
+
 	@Autowired
 	private LoginUserService lus;
-	
+
 	@RequestMapping("login.action")
 	@ResponseBody
-	public Map<String, Object> login(LoginUser user,HttpSession session){
+	public Map<String, Object> login(String userName,String pwd,String checkCode,HttpSession session){
+		Map<String, Object>json = new HashMap<String, Object>();
+		String code = String.valueOf(session.getAttribute("code"));
+		if(code==null || !code.equalsIgnoreCase(checkCode)) {
+			json.put("flag", false);
+			return json;
+		}
+		
+		LoginUser user = new LoginUser();
+		user.setUserName(userName);
+		user.setPwd(pwd);
 		System.out.println("进入user控制器");
 		user = lus.login(user);
-		Map<String, Object>json = new HashMap<String, Object>();
+		
 		if(user!=null) {
 			json.put("flag",true);
 			session.setAttribute("loginUser", user);
@@ -36,13 +53,30 @@ public class LoginUserController {
 		}
 		return json;
 	}
-	
+
 	@RequestMapping("zs_index.action")
 	public ModelAndView zs_index() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("zs_index");
 		return mv;
 	}
-	 
-	
+
+	@RequestMapping("code.action")
+	//显示验证码，把验证码放入session中
+	public void showCode(HttpSession session,HttpServletResponse response) throws IOException {
+		
+		String code = GraphicHelper.createCode();
+		response.setContentType("image/jpg");
+		final int width = 180; // 图片宽度
+		final int height = 40; // 图片高度
+		final String imgType = "jpg"; // 指定图片格式 (不是指MIME类型)
+		final OutputStream output = response.getOutputStream(); // 获得可以向客户端返回图片的输出流
+		// (字节流)
+		// 创建验证码图片并返回图片上的字符串
+		BufferedImage image = GraphicHelper.create(width, height, imgType,code);
+		ImageIO.write(image, imgType, output);
+		System.out.println(code);
+		session.setAttribute("code", code);
+	}
+
 }
