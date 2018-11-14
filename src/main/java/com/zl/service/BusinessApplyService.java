9a75709f2a5ab861dcd538f9fa0656173c67ccdc
liu <707546275@qq.com>
2018-11-14 14:07:53 +0800

    显示下拉列表的

--- src/main/java/com/zl/service/BusinessApplyService.java
+++ src/main/java/com/zl/service/BusinessApplyService.java
@@ -2,6 +2,8 @@ package com.zl.service;
 
 
 import com.zl.pojo.BusinessApply;
+import com.zl.pojo.Examine;
+import com.zl.pojo.PageBean;
 
 import java.util.List;
 
@@ -11,4 +13,8 @@ public interface BusinessApplyService {
     int updateFlag5(String serialno, String customername, String remarks,Integer type);
 
     List<BusinessApply> findAllByFlag5(Integer type);
+
+    Examine findCustomer(String serialno);
+
+    PageBean<BusinessApply> PagingQuery(Integer currPage,Integer pageSize,Integer type);
 }
