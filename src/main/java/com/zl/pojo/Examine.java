533c30a526b29f8897f37eae6a0b07982c2b80d9
liu <707546275@qq.com>
2018-11-15 20:22:30 +0800

    显示下拉列表的

---
+++ src/main/java/com/zl/pojo/Examine.java
@@ -0,0 +1,316 @@
+package com.zl.pojo;
+
+public class Examine {
+//    流水号
+    private String serialno;
+//    姓名
+    private String customername;
+//    年龄：birthday
+    private String birthday;
+//    性别：sex
+    private String sex;
+//    身份证号：certid18
+    private String certid18;
+//手机号码：mobiletelephone
+    private String mobiletelephone;
+//电子邮箱：emailadd
+    private String emailadd;
+//最高学历：eduexperience
+    private String eduexperience;
+//毕业学校：edurecord
+    private String edurecord;
+//职业/单位：occupation
+    private String occupation;
+//职称：position
+    private String position;
+//婚姻状况：marriage
+    private String marriage;
+//===============
+//现单位工作时间：workzip
+    private String workzip;
+//配偶职业/单位：workcorp
+    private String workcorp;
+//配偶职位：worktel
+    private String worktel;
+//负债比率：staff
+    private String staff;
+//保险：intro
+    private String intro;
+//总体印象：remark
+    private String remark;
+//个人贷款记录年限：country
+    private String country;
+//个人贷款累计有效违约率：creditfarmer
+    private String creditfarmer;
+//家庭年收入：familymonthincome
+    private String familymonthincome;
+//==================
+//家庭人口数：population
+    private String population;
+//居住地址：familyadd
+    private String familyadd;
+//居住状况：familystatus
+    private String familystatus;
+//收入来源：incomesource
+    private String incomesource;
+//个人年收入：yearincome;
+    private String yearincome;
+//户籍地址：nativeplace
+    private String nativeplace;
+//贷款卡号：loancardno
+    private String loancardno;
+//信用等级：creditlevel
+    private String creditlevel;
+//更新日期：updatedate
+    private String updatedate;
+//评估日期：evaluatedate
+    private String evaluatedate;
+
+    public String getSerialno() {
+        return serialno;
+    }
+
+    public void setSerialno(String serialno) {
+        this.serialno = serialno;
+    }
+
+    public String getCustomername() {
+        return customername;
+    }
+
+    public void setCustomername(String customername) {
+        this.customername = customername;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCertid18() {
+        return certid18;
+    }
+
+    public void setCertid18(String certid18) {
+        this.certid18 = certid18;
+    }
+
+    public String getMobiletelephone() {
+        return mobiletelephone;
+    }
+
+    public void setMobiletelephone(String mobiletelephone) {
+        this.mobiletelephone = mobiletelephone;
+    }
+
+    public String getEmailadd() {
+        return emailadd;
+    }
+
+    public void setEmailadd(String emailadd) {
+        this.emailadd = emailadd;
+    }
+
+    public String getEduexperience() {
+        return eduexperience;
+    }
+
+    public void setEduexperience(String eduexperience) {
+        this.eduexperience = eduexperience;
+    }
+
+    public String getEdurecord() {
+        return edurecord;
+    }
+
+    public void setEdurecord(String edurecord) {
+        this.edurecord = edurecord;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getMarriage() {
+        return marriage;
+    }
+
+    public void setMarriage(String marriage) {
+        this.marriage = marriage;
+    }
+
+    public String getWorkzip() {
+        return workzip;
+    }
+
+    public void setWorkzip(String workzip) {
+        this.workzip = workzip;
+    }
+
+    public String getWorkcorp() {
+        return workcorp;
+    }
+
+    public void setWorkcorp(String workcorp) {
+        this.workcorp = workcorp;
+    }
+
+    public String getWorktel() {
+        return worktel;
+    }
+
+    public void setWorktel(String worktel) {
+        this.worktel = worktel;
+    }
+
+    public String getStaff() {
+        return staff;
+    }
+
+    public void setStaff(String staff) {
+        this.staff = staff;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCreditfarmer() {
+        return creditfarmer;
+    }
+
+    public void setCreditfarmer(String creditfarmer) {
+        this.creditfarmer = creditfarmer;
+    }
+
+    public String getFamilymonthincome() {
+        return familymonthincome;
+    }
+
+    public void setFamilymonthincome(String familymonthincome) {
+        this.familymonthincome = familymonthincome;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    public String getFamilyadd() {
+        return familyadd;
+    }
+
+    public void setFamilyadd(String familyadd) {
+        this.familyadd = familyadd;
+    }
+
+    public String getFamilystatus() {
+        return familystatus;
+    }
+
+    public void setFamilystatus(String familystatus) {
+        this.familystatus = familystatus;
+    }
+
+    public String getIncomesource() {
+        return incomesource;
+    }
+
+    public void setIncomesource(String incomesource) {
+        this.incomesource = incomesource;
+    }
+
+    public String getYearincome() {
+        return yearincome;
+    }
+
+    public void setYearincome(String yearincome) {
+        this.yearincome = yearincome;
+    }
+
+    public String getNativeplace() {
+        return nativeplace;
+    }
+
+    public void setNativeplace(String nativeplace) {
+        this.nativeplace = nativeplace;
+    }
+
+    public String getLoancardno() {
+        return loancardno;
+    }
+
+    public void setLoancardno(String loancardno) {
+        this.loancardno = loancardno;
+    }
+
+    public String getCreditlevel() {
+        return creditlevel;
+    }
+
+    public void setCreditlevel(String creditlevel) {
+        this.creditlevel = creditlevel;
+    }
+
+    public String getUpdatedate() {
+        return updatedate;
+    }
+
+    public void setUpdatedate(String updatedate) {
+        this.updatedate = updatedate;
+    }
+
+    public String getEvaluatedate() {
+        return evaluatedate;
+    }
+
+    public void setEvaluatedate(String evaluatedate) {
+        this.evaluatedate = evaluatedate;
+    }
+}
