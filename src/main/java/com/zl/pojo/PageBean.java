8fa2ab082fe2a8dd8b216fd1e926719b642187aa
liu <707546275@qq.com>
2018-11-15 20:22:47 +0800

    增加了分页

---
+++ src/main/java/com/zl/pojo/PageBean.java
@@ -0,0 +1,62 @@
+package com.zl.pojo;
+
+import java.util.List;
+
+public class PageBean<T> {
+    private Integer currPage;//当前页数
+    private Integer pageSize;//每页显示的记录数
+    private Integer totalCount;//总记录数
+    private Integer totalPage;//总页数
+    private List<T> lists;//每页的显示的数据
+
+    public Integer getCurrPage() {
+        return currPage;
+    }
+
+    public void setCurrPage(Integer currPage) {
+        this.currPage = currPage;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(Integer totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public Integer getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(Integer totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public List<T> getLists() {
+        return lists;
+    }
+
+    public void setLists(List<T> lists) {
+        this.lists = lists;
+    }
+
+    @Override
+    public String toString() {
+        return "PageBean{" +
+                "currPage=" + currPage +
+                ", pageSize=" + pageSize +
+                ", totalCount=" + totalCount +
+                ", totalPage=" + totalPage +
+                ", lists=" + lists +
+                '}';
+    }
+}
